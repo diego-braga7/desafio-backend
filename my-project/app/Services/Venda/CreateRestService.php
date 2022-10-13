cd6fd26a98efc14f277fb8de7de60dc4e0a0bfe7
Diego Braga <diego.braga@gmail.com>
2022-10-13 09:44:02 -0300

    feat: implements querys sell

--- my-project/app/Services/Venda/CreateRestService.php
+++ my-project/app/Services/Venda/CreateRestService.php
@@ -5,12 +5,15 @@ namespace App\Services\Venda;
 use App\DomainModel\ProdutoVenda;
 use App\DomainModel\Venda;
 use App\Enum\HttpResponses;
+use App\Enum\StatusVenda;
+use App\Models\Pdv;
 use App\Models\ProdutosVenda;
 use App\Services\Contract\AbstractRestService;
 use App\Services\Produto\Contract\IValidateService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
 
 class CreateRestService extends AbstractRestService
 {
@@ -23,6 +26,10 @@ class CreateRestService extends AbstractRestService
     public function dispatch(array $data, mixed $id = null): JsonResponse|JsonResource
     {
         $produtos = $this->produto->get($data['produtos']);
+
+        if(count($data['produtos']) != $produtos->count()){
+            return $this->jsonResponse("Produto não encontrado.", HttpResponses::UNPROCESSABLE_ENTITY);
+        }
         $produtosVendasEntity = $produtos->map(function (ProdutoVenda $produtoVenda) {
             $produtoVenda->setValorTotal($produtoVenda->getValorUnitario() * $produtoVenda->getQuantidade());
             return $produtoVenda;
@@ -32,7 +39,15 @@ class CreateRestService extends AbstractRestService
         $venda->setPdvId($data['pdv_id']);
 
         $venda->setValor($this->calculateValueTotal($produtos));
+
+        $debit = $this->getDebitValue($venda->getPdvId());
+
+       if(!$this->canCreateSale($debit, $venda)){
+           return $this->jsonResponse("Limite estourado.", HttpResponses::UNPROCESSABLE_ENTITY);
+       }
+
         $vendaFinal = new Venda(\App\Models\Venda::create($venda->toArray()));
+
 
         $produtosVendasEntity->map(function (ProdutoVenda $produtoVenda) use ($vendaFinal) {
             $produtoVenda->setVendaId($vendaFinal->getId());
@@ -45,13 +60,20 @@ class CreateRestService extends AbstractRestService
 
     private function getDebitValue(int $pdvId) : float
     {
-        return 0.0;
+       $debit = DB::select("
+            SELECT SUM(valor) AS debit FROM venda WHERE pdv_id = :pdvId AND `status` != :status AND quitado = :quitado
+        ", [
+            'pdvId' => $pdvId,
+            'status' => StatusVenda::CANCELADO->value,
+            'quitado' => 0,
+        ]);
+       return (float) current($debit)->debit;
     }
 
     private function canCreateSale(float $debit, Venda $venda) : bool
     {
-        $limit = 100.0; //find in pdv database
-        return ($debit + $venda->getValor()) >= $limit;
+        $limit = Pdv::find($venda->getPdvId())->limite_venda_pos;
+        return ($debit + $venda->getValor()) < $limit;
     }
 
     private function calculateValueTotal(Collection $collection): float
